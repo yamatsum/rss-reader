a2eea7da298b7ea1723f49faa05ca1190ac98dd3
yamatsum <sy12250@gmail.com>
2018-10-07 17:27:14 +0900

    Automatic shaping by prettier

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -1,51 +1,39 @@
-import {
-  Body,
-  Content,
-  List,
-  ListItem,
-  Thumbnail,
-  Text,
-} from "native-base";
+import { Body, Content, List, ListItem, Thumbnail, Text } from "native-base";
 import * as React from "react";
-import {
-  RefreshControl,
-  AsyncStorage,
-} from 'react-native';
-import {
-  Actions,
-} from 'react-native-router-flux';
+import { RefreshControl, AsyncStorage } from "react-native";
+import { Actions } from "react-native-router-flux";
 
 class ListComponent extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
       rss: [],
-      refreshing: false,
-    }
+      refreshing: false
+    };
     this.setRss = this.setRss.bind(this);
   }
 
   setRss(json: any) {
     this.setState({
-      rss: json.items,
+      rss: json.items
     });
   }
 
   fetchData = () => {
-    console.log('hoge');
-    return 'ok';
-  }
+    console.log("hoge");
+    return "ok";
+  };
 
   _onRefresh = async () => {
-    this.setState({refreshing: true});
-    console.log('hoge');
-    this.setState({refreshing: false});
+    this.setState({ refreshing: true });
+    console.log("hoge");
+    this.setState({ refreshing: false });
     // this.fetchData().then(() => {
     //   this.setState({refreshing: false});
     // });
 
     try {
-      const value = await AsyncStorage.getItem('bookmarks');
+      const value = await AsyncStorage.getItem("bookmarks");
       if (value !== null) {
         // We have data!!
         console.log(value);
@@ -53,20 +41,20 @@ class ListComponent extends React.Component<any, any> {
     } catch (error) {
       // Error retrieving data
     }
-  }
+  };
 
   componentWillMount() {
-    const parseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=';
-    const rssUrl = 'http://jojosoku.com/feed';
+    const parseUrl = "https://api.rss2json.com/v1/api.json?rss_url=";
+    const rssUrl = "http://jojosoku.com/feed";
     fetch(parseUrl + rssUrl)
-    .then(response => response.json())
-    .then((json) => {
-      if (json.status === 'ok') {
-        this.setRss(json);
-      }else {
-        console.log("failed");
-      }
-    });
+      .then(response => response.json())
+      .then(json => {
+        if (json.status === "ok") {
+          this.setRss(json);
+        } else {
+          console.log("failed");
+        }
+      });
   }
 
   render() {
@@ -76,20 +64,24 @@ class ListComponent extends React.Component<any, any> {
           <RefreshControl
             refreshing={this.state.refreshing}
             onRefresh={this._onRefresh}
-            />
-            }
+          />
+        }
       >
         <List
           dataArray={this.state.rss}
-          renderRow={(rss) =>
-            <ListItem onPress={() => { Actions.ArticleScreen({ rss }); }}>
+          renderRow={rss => (
+            <ListItem
+              onPress={() => {
+                Actions.ArticleScreen({ rss });
+              }}
+            >
               <Thumbnail square size={80} source={{ uri: rss.thumbnail }} />
               <Body>
-                <Text>{ rss.title }</Text>
+                <Text>{rss.title}</Text>
               </Body>
             </ListItem>
-            }>
-          </List>
+          )}
+        />
       </Content>
     );
   }
