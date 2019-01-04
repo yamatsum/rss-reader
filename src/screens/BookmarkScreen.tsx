1aa863f7d444780d70fb33e4163b8b3f95a6d971
yamatsum <sy12250@gmail.com>
2019-01-04 20:29:22 +0900

    Use listcomponent also in bookmark

--- src/screens/BookmarkScreen.tsx
+++ src/screens/BookmarkScreen.tsx
@@ -1,36 +1,30 @@
-import { Body, Content, List, ListItem, Thumbnail, Text } from "native-base";
+import {
+  Container,
+  Tab,
+  Tabs,
+  View,
+  Header,
+  Left,
+  Right,
+  Body,
+  Title
+} from "native-base";
 import * as React from "react";
 import { RefreshControl, AsyncStorage } from "react-native";
 import { Actions } from "react-native-router-flux";
+import ListComponent from "../containers/ListComponent";
 
 class BookmarkScreen extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      rss: [],
+      rss: [{ title: "fuga" }],
       refreshing: false
     };
   }
 
   fetchData = () => {
     return "ok";
-  };
-
-  _onRefresh = async () => {
-    // this.setState({ refreshing: true });
-    // this.setState({ refreshing: false });
-    // this.fetchData().then(() => {
-    //   this.setState({refreshing: false});
-    // });
-    // try {
-    //   const value = await AsyncStorage.getItem("bookmarks");
-    //   if (value !== null) {
-    //     // We have data!!
-    //     console.log(value);
-    //   }
-    // } catch (error) {
-    // Error retrieving data
-    // }
   };
 
   _retrieveData = async () => {
@@ -52,30 +46,11 @@ class BookmarkScreen extends React.Component<any, any> {
 
   render() {
     return (
-      <Content
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.refreshing}
-            onRefresh={this._onRefresh}
-          />
-        }
-      >
-        <List
-          dataArray={this.state.rss}
-          renderRow={rss => (
-            <ListItem
-              onPress={() => {
-                Actions.ArticleScreen({ rss });
-              }}
-            >
-              <Thumbnail square size={80} source={{ uri: rss.thumbnail }} />
-              <Body>
-                <Text>{rss.title}</Text>
-              </Body>
-            </ListItem>
-          )}
-        />
-      </Content>
+      <Container>
+        <View style={{ flex: 1 }}>
+          <ListComponent rss={this.state.rss} />
+        </View>
+      </Container>
     );
   }
 }
